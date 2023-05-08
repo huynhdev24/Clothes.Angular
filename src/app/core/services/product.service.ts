04b16f3cd9c0438146e06c1842eb417eb3a2e72c
huynhdev24 <huynh.dev.24@gmail.com>
2023-05-08 17:43:02 +0700

    add women images

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -3,21 +3,23 @@ import { HttpClient } from "@angular/common/http";
 import { BehaviorSubject, map, Observable, of} from "rxjs";
 import { Product } from "../models/product";
 import { productsPageSize } from "../../../environments/environment";
+// import { EnvironmentUrlService } from '../services/environment.service';
+// import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
   private products$ = new BehaviorSubject<Product[]>([]);
-
+  // private envUrl = environment;
   constructor(private httpClient: HttpClient) { }
 
   public getFeaturedProducts(): Observable<Product[]> {
-    return this.httpClient.get<Product[]>('api/Products/Featured');
+    return this.httpClient.get<Product[]>('https://localhost:7288/api/Products/Featured');
   }
 
   public getProductsPaged(page: number, gender: string): Observable<Product[]> {
-    return this.httpClient.get<Product[]>(`api/Products?page=${page}&pageSize=${productsPageSize}&gender=${gender}`);
+    return this.httpClient.get<Product[]>(`https://localhost:7288/api/Products?page=${page}&pageSize=${productsPageSize}&gender=${gender}`);
   }
 
   public getProductById(id: number): Observable<Product> {
@@ -25,7 +27,7 @@ export class ProductService {
       return of(this.products$.value.find(product => product.id === id) as Product);
     }
 
-    return this.httpClient.get<Product>(`api/Products/${id}`)
+    return this.httpClient.get<Product>(`https://localhost:7288/api/Products/${id}`)
       .pipe(map(product => {
         this.products$.next([...this.products$.value, product]);
         return product;
@@ -33,6 +35,6 @@ export class ProductService {
   }
 
   public getProductsCountByGender(gender: string): Observable<number> {
-    return this.httpClient.get<number>(`api/Products/Count/${gender}`);
+    return this.httpClient.get<number>(`https://localhost:7288/api/Products/Count/${gender}`);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    production: false
+    production: false,
+    urlAddress: 'https://localhost:7288'
   };
   
   export const productsPageSize = 8;
