8cfe3a6aad9dd3375d5979bb8311e149a1499739
huynhdev24 <huynh.dev.24@gmail.com>
2023-05-08 15:01:07 +0700

    fix product

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/core/services/product.service';
+import { Product } from 'src/app/core/models/product';
+import { ActivatedRoute, ParamMap, Router, RouterModule } from '@angular/router';
+import { Unsubscriber } from 'src/app/shared/helpers/unsubscriber';
+import {catchError, EMPTY, map, Observable, switchMap, takeUntil } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-product',
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export default class HomeComponent extends Unsubscriber implements OnInit {
+  product$!:Observable<Product>;
+  selectedVariant: string | undefined;
 
+  constructor(privateService: ProductService, private route: ActivatedRoute, private router: Router) {
+    super()
+  }
+
+  ngOnInit(): void {
+    this.product$ = this.route.paramMap
+      .pipe(
+        map((params: ParamMap) => Number(params.get('id'))),
+        switchMap((id: number) => {
+          return this.productService.getProductById(id)
+            .pipe(catchError(() => {
+              this.router.navigate(['/']);
+              return EMPTY;
+            }))
+          })
+      );
+  }
+
+  selectVariant(variant: string): void {
+    this.selectedVariant = variant;
+  }
 }
