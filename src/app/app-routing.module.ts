28955dcf60e685a6efc01a3ea583c33899782c15
huynhdev24 <huynh.dev.24@gmail.com>
2023-05-08 14:30:21 +0700

    add config tailwindcss and app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home.component'),
+    title: 'Clothes - Home'
+  },
+  {
+    path: 'product/:id',
+    loadComponent: () => import('./pages/product/product.component')
+  },
+  {
+    path: 'products/:gender',
+    loadComponent: () => import('./pages/products/products.component')
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [CommonModule,RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
